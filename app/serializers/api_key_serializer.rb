40fe6d62689d7d51327bfda542103572d195758a
Brad Humphrey <brad@instructure.com>
2014-01-10 16:54:52 -0700

    adding active_model/serializer require

--- app/serializers/api_key_serializer.rb
+++ app/serializers/api_key_serializer.rb
@@ -1,3 +1,5 @@
+require 'active_model/serializer'
+
 module Skydrive
   class ApiKeySerializer < ActiveModel::Serializer
     attributes :id, :user_id, :access_token
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,5 @@
+require 'active_model/serializer'
+
 module Skydrive
   class UserSerializer < ActiveModel::Serializer
     attributes :id, :name, :username, :email
