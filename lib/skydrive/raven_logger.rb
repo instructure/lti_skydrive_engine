4203bb2d7c035e96bb66549084aef25a2ac450d5
defektive <sirbradleyd@gmail.com>
2015-05-11 16:03:17 -0600

    Changes for more tests

--- lib/skydrive/raven_logger.rb
+++ lib/skydrive/raven_logger.rb
@@ -5,6 +5,7 @@ module Skydrive
     ENV_KEY = "RAVEN_SKYDRIVE_DSN"
 
     def self.capture_exception(error)
+      return if Rails.env.test?
       if (ENV[ENV_KEY])
         config = Raven.configuration
         config.dsn = ENV.fetch(ENV_KEY)
